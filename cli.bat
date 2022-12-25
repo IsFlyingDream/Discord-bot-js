c5fdc489f7dd533893b9658cd26f9247777ed98e
IsFlyingDream <ruby29922026@gmail.com>
2022-12-25 12:14:03 +0800

    機器人程式放到git上

---
+++ cli.bat
@@ -0,0 +1,6 @@
+
+echo "本文件在命令函式才有用到"
+echo "321"
+
+cd DiscordChatExporter.Cli/
+dotnet DiscordChatExporter.Cli.dll export -t "MTA1MzY0Njc1NTk2ODg1NjA5NA.GDhrzs.VHhJVy6Y7V7qW9eMTeFGP6-gO4jO98Yt1w92YA" -c 1053640153308086292 -o "\Desktop\%G\%C.html" 
