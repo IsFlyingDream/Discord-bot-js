fe20263e1460bf585df1468933641bd9624d42e9
IsFlyingDream <ruby29922026@gmail.com>
2022-12-28 05:51:51 +0000

    註解$save指令可以設定時間的功能。原因1.伺服器時間與本地時間不一致2.若沒有判斷，機器人容易出現故障而掉線。

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 //本文件關鍵字為：$save
+//以下方法因為$save 加上時間有許多問題：內部機器時間與Discord時間不同、需要加判斷式、容易玩壞的問題，因此僅有$save的功能
 //const { channel } = require('diagnostics_channel');
 const { Client, GatewayIntentBits } = require('discord.js');
 
-//const keepAlive = require(`./server`);
+const keepAlive = require(`./server`);//保持機器人清醒
+
 //const { token } = require('./token.json');
 //剛加前綴字
 //const { prefix } = require('./token.json');
@@ -12,6 +14,9 @@ const { Client, GatewayIntentBits } = require('discord.js');
 //dotenv.config();
 const prefix = process.env['PREFIX'];
 const token = process.env['TOKEN'];
+
+var fs = require("fs"),
+  path = require("path");
 
 
 //const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -46,12 +51,40 @@ client.on('messageCreate', async (message) => {//從訊息上辨認
     const keyWord = prefixText.substr(1);//砍掉第一個前綴字
     const rules = /save+/;
 
+    //刪除檔案的function
+    function deleteFile(url, name) {
+      var files = [];
+
+      if (fs.existsSync(url)) {    //判断给定的路径是否存在
+
+        files = fs.readdirSync(url);    //返回文件和子目录的数组
+
+        files.forEach(function(file, index) {
+
+          var curPath = path.join(url, file);
+
+          if (fs.statSync(curPath).isDirectory()) { //同步读取文件夹文件，如果是文件夹，则函数回调
+            deleteFile(curPath, name);
+          } else {
+
+            if (file.indexOf(name) > -1) {    //是指定文件，则删除
+              fs.unlinkSync(curPath);
+              console.log("刪除文件：" + curPath);
+            }
+          }
+        });
+      } else {
+        console.log("給予的路徑不存在！");
+      }
+    }
+
+    //  以下是判斷式  ------------------------
     if (keyWord === `save`) {
       //(最新)執行此方法>>cli.bat改成字串傳值，不同段 以&&相接-------------    
       //*
       var exec = require('child_process').exec;
       function execute() {
-        var cmd = 'cd DiscordChatExporter.Cli';
+        let cmd = 'cd DiscordChatExporter.Cli';
         cmd += ' && dotnet DiscordChatExporter.Cli.dll export -t \"' + token + '\" -c ' + message.channel.id + ' -o "%G_%C.html" ';
         exec(cmd,
           (error, stdout, stderr) => {
@@ -60,62 +93,69 @@ client.on('messageCreate', async (message) => {//從訊息上辨認
             if (error !== null) {
               console.log(`exec error: ${error}`);
             }
-            
+
             // Send a local file發送本地文件      
             message.channel.send({
               files: [{
-                attachment: './DiscordChatExporter.Cli/'+message.guild.name+'_'+message.channel.name+'.html',
-                name: message.guild.name+'_'+message.channel.name+'.html'
+                attachment: './DiscordChatExporter.Cli/' + message.guild.name + '_' + message.channel.name + '.html',
+                name: message.guild.name + '_' + message.channel.name + '.html'
               }]
-            }).then(msg => {
-              //aaaaaaaa
+            }).then(msg => {              //aaaaaaaa
+              //檔案傳送完以後就刪除本機檔案
+              deleteFile('DiscordChatExporter.Cli/', message.guild.name + '_' + message.channel.name + '.html');
+
             }).catch(console.error);
 
           });
+
       }
       execute();
       //*/
-      console.log(message.guild.name +' ');
-
       //*/
 
 
       message.reply('全部時段，儲存中...');//機器人的回應
     }
+    /*以下因為時間判斷若有打錯，系統就會當掉，並且replit時間與內部時間不同，有錯誤情況發生，因此不使用
     else if (rules.test(keyWord) === true) {
       const timeString = keyWord.substr(4);//取得後續字串的時間
       console.log(timeString);
 
       //(最新)執行此方法>>cli.bat改成字串傳值，不同段 以&&相接-------------   
-      //*
-      var exec = require('child_process').exec;
-      function execute() {
-        var cmd = 'cd DiscordChatExporter.Cli';
-        cmd += ' && dotnet DiscordChatExporter.Cli.dll export -t \"' + token + '\" -c ' + message.channel.id + '  ' + timeString + '  -o %G_%C.html ';
-        exec(cmd,
+     
+      let exec = require('child_process').exec;
+      function execute2() {
+        
+        let cmd2 = 'cd DiscordChatExporter.Cli';
+        cmd2 += ' && dotnet DiscordChatExporter.Cli.dll export -t \"' + token + '\" -c ' +  message.channel.id + ' ' + timeString  +  '  -o "%G_%C.html" ';
+
+          exec(cmd2,
           (error, stdout, stderr) => {
             console.log(`${stdout}`);
             console.log(`${stderr}`);
             if (error !== null) {
               console.log(`exec error: ${error}`);
             }
-          });
 
-         // Send a local file發送本地文件      
+            // Send a local file發送本地文件      
             message.channel.send({
               files: [{
-                attachment: './DiscordChatExporter.Cli/'+message.guild.name+'_'+message.channel.name+'.html',
-                name: message.guild.name+'_'+message.channel.name+'.html'
+                attachment: './DiscordChatExporter.Cli/' + message.guild.name + '_' + message.channel.name + '.html',
+                name: message.guild.name + '_' + message.channel.name + '.html'
               }]
             }).then(msg => {
               //aaaaaaaa
+              //檔案傳送完以後就刪除本機檔案
+               deleteFile('DiscordChatExporter.Cli/', message.guild.name + '_' + message.channel.name + '.html');
+              
             }).catch(console.error);
-        
+              
+          });
       }
-      execute();
-      //*/
+      execute2();
+      
       message.reply('指定時段，儲存中...');//機器人的回應
-    }
+    }*/
   }
 });
 
@@ -163,5 +203,6 @@ client.on('interactionCreate', async interaction => {//啟動指令
 
 
 //client.login(token);
+keepAlive();
 client.login(token);
-//keepAlive();
+
